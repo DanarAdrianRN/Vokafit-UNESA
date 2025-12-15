ea689ce0bf2df68a06c61fed0d15ca1152b6be73
Danar Adrian Ridho Nugroho <124489939+DanarAdrianRN@users.noreply.github.com>
2025-12-15 15:48:12 +0700

    update-main1.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
-
